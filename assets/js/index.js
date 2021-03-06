47f53f812d21259d128a01788af2c5f66ece818e
stefanociarelli <stefanochiarelli17@gmail.com>
2021-03-06 18:23:20 -0300

    Cambie el carrusel con glide.js, hize el navbar responsive y cambie el tamano del footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,11 @@ const navbarMenu = document.querySelector('#nav-links');
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active')
 });
+
+
+
+const homeTab = document.querySelector('#tabHome');
+
+homeTab.addEventListener('click', () => {
+    homeTab.style.color = "red";
+});
